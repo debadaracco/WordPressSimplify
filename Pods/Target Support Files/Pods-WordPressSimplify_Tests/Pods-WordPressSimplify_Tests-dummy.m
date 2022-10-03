2293898bc2ecb7e3b622f55df07d03ad109931d8
Diego Badaracco <debadaracco@gmail.com>
2022-10-03 00:01:10 -0300

    add pods folder in gitignore and remove wrong content uploaded

--- Pods/Target Support Files/Pods-WordPressSimplify_Tests/Pods-WordPressSimplify_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WordPressSimplify_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_WordPressSimplify_Tests
-@end
--- Pods/Target Support Files/WordPressSimplify/WordPressSimplify-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_WordPressSimplify : NSObject
-@end
-@implementation PodsDummy_WordPressSimplify
-@end
